3da7da310cc9ca6f32e1843b29d773d2a6bb66b6
Jozef Maloch <Jozefmaloch@gmail.com>
2018-03-31 16:16:02 +0200

    organized the structure of serverside to its own folder 'server'; added react router to the index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,18 @@ import './index.css';
 import './header.css';
 import './footer.css';
 import App from './App';
+import ExampleAdd from './example/exampleAdd';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path='/' component={App} />
+      <Route path='/add' component={ExampleAdd} />
+    </div>
+  </Router>,
+
+  document.getElementById('root')
+);
 registerServiceWorker();
